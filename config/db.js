0db1293098073fc73852a9aae5767a19db98d07e
lou <dustyfingers01257@gmail.com>
2021-04-05 10:26:38 -0600

    db changes for deploy

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 // assign db path and options
-const dbPath = "mongodb://localhost/beat-store-db";
+const dbPath = process.env.PORT ? "mongodb+srv://dbUser:iZ7XEQlrc02Q9XSE@louiemadeit-testdb.aotsm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority": "mongodb://localhost/beat-store-db";
 const dbOpts = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
