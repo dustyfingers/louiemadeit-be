19155746ae1a7f9df9935edce3b2fb36888fd229
lou <dustyfingers01257@gmail.com>
2021-05-13 16:30:35 -0600

    removed console logs

--- api/routes/auth/auth.js
+++ api/routes/auth/auth.js
@@ -57,7 +57,6 @@ router.post("/sign-up", (req, res, next) => {
 
 // fetch current user
 router.get("/current-user", (req, res) => {
-    console.log({user: req.user});
     if (req.user) {
         res.send({ 
             user: { 
